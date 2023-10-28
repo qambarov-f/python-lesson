127f0481d59ada2845f077f5a382f52b2a47b6a5
Feyruz Gambarov <qemberov.f1949@gmail.com>
2023-10-28 07:42:47 +0400

    lesson 2 completed

---
+++ index.py
@@ -0,0 +1,16 @@
+# print("Hello  \tWorld")
+
+# a = 5
+# b = 2.5
+# c = "Python"
+# d = False
+
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+a = "Python"
+
+print(a.upper())
---
+++ lesson2.py
@@ -0,0 +1,96 @@
+# task1
+
+# isim = input("Please enter name: ")
+
+
+# if isim == "Aslan":
+#     print("Aslan emi ogludur")
+# elif isim == "Imran":
+#     print("Imran dayi ogludur")
+# elif isim == "Afaq":
+#     print("Afaq bibi qizidir")
+# elif isim == "Uzeyir":
+#     print("Uzeyir xala ogludur")
+# elif isim == "Shahin":
+#     print("Shahin yaxin dostdur")
+# elif isim == "":
+#     print("Siz ad daxil etmediz")
+# else:
+#     print("${isim} kimdir? Bu adı tanımıram!")
+
+
+# task2
+
+# login = input("Login:\n")
+# parol = input("Parol:\n")
+
+
+# if login == "test" and parol == "12345":
+#     print("# Xosh geldiniz")
+# elif login == "test":
+#     print("# Parol yanlışdır")
+# elif parol == "12345":
+#     print("# Login yanlışdır")
+# elif login == "" and parol == "":
+#     print("# Login ve parol daxil etmediniz")
+# elif login == "":
+#     print("# Login daxil etmediniz")
+# elif parol == "":
+#     print("# Parol daxil etmediniz")
+# else:
+#     print("# Login ve parol yanlışdır")
+
+
+# task3
+
+# num1 = int(input("Enter first num: "))
+# num2 = int(input("Enter second num: "))
+# num3 = int(input("Enter third num: "))
+
+# max_Num = max(num1, num2, num3)
+
+
+# print("The max number is:", max_Num)
+
+
+# -----------------------
+# task4
+
+# boy = float(input("Boyunuzu daxil edin: "))
+# kilo = float(input("Kilonuzu daxil edin: "))
+
+
+# bki = kilo / (boy * boy)
+
+
+# if bki < 18.5:
+#     sum = "Zəif"
+# elif 18.5 <= bki < 25:
+#     sum = "Normal"
+# elif 25 <= bki < 30:
+#     sum = "Kilolu"
+# else:
+#     sum = "Obez"
+
+# print(sum)
+
+# ----------------------------
+# task5
+
+
+# TASK6
+
+test_list = [5, 6, [], 3, [], [], 9]
+
+
+test_list = [x for x in test_list if x]
+
+for num in test_list:
+    print(num)
+
+
+# task7
+
+
+
+
