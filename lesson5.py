473cbb3f0f760911442874c346b115713114f3ac
Feyruz Gambarov <qemberov.f1949@gmail.com>
2023-11-02 09:16:34 +0400

    lesson 5 completed

---
+++ lesson5.py
@@ -0,0 +1,38 @@
+# task1
+
+
+# try:
+#     enteredOperation = int(input('Enter \nMultiplication (1), Reduce (2), Addition (3), Divide (4): '))
+#     enteredNumber1 = int(input('Please enter number: '))
+#     enteredNumber2 = int(input('Please enter number: '))
+# except:
+#     print('Please enter only numbers')
+
+# def calculatorFunction():
+#     if enteredOperation == 1:
+#         print(f'{enteredNumber1} * {enteredNumber2} = ' + str((enteredNumber1 * enteredNumber2)))
+#     elif enteredOperation == 2:
+#         print(f'{enteredNumber1} - {enteredNumber2} = ' + str((enteredNumber1 - enteredNumber2)))
+#     elif enteredOperation == 3:
+#         print(f'{enteredNumber1} + {enteredNumber2} = ' + str((enteredNumber1 + enteredNumber2)))
+#     elif enteredOperation == 4:
+#         try:
+#             print(f'{enteredNumber1} / {enteredNumber2} = ' + str((enteredNumber1 / enteredNumber2)))
+#         except:
+#             print("You can't divide a number by zero!")
+#     else:
+#         print('Such a process does not exist...')
+
+# calculatorFunction()
+
+
+# task2
+
+# num = 1
+
+# while num <= 10:
+#     willBeIncreased = 1
+#     while willBeIncreased <= 10:
+#         print(f'{num} * {willBeIncreased} = ' + str((num * willBeIncreased)))
+#         willBeIncreased += 1
+#     num += 1
